b1b368accdd31f12ef4c397e936d0d095db70ccc
frankiewei <frankiewei23@gmail.com>
2019-09-27 20:19:12 +1000

    Update trip_update.py

--- trip_update.py
+++ trip_update.py
@@ -6,7 +6,7 @@ from nested_lookup import nested_lookup
 from nested_lookup import get_all_keys
 from nested_lookup import get_occurrence_of_key, get_occurrence_of_value
 import json
-from protofbuf import tfnsw_gtfs_realtime_pb2
+from protobuf import tfnsw_gtfs_realtime_pb2
 
 api_key = 'apikey EXmt0Pl6OWxy2OZUo19fus2PYKsMd4F6G7W8'
 
